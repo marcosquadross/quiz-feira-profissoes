26a2c893d72488a7bcfb6db50a1f4d448ad25e6e
marcosquadross <Marcosv.quadros@outlook.com>
2026-05-31 13:03:14 -0300

    fix: configurações cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,16 +12,11 @@ export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
   app.enableCors({
-    origin: [
-      'http://172.16.255.204:8888', 
-      'http://200.134.18.85:8888', 
-      'http://localhost:5173', 
-      'http://localhost:8888'
-    ],
+    origin: process.env.FRONTEND_URL?.split(','),
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
-  });  
+  });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.SERVER_PORT, '0.0.0.0');
 
